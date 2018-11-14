b64e0a4e2b5520931c6ccb333be2743a1004e143
highpreef <36968147+highpreef@users.noreply.github.com>
2018-11-14 14:40:17 +0000

    Update MoneyTest.java

--- MoneyTest.java
+++ MoneyTest.java
@@ -26,7 +26,75 @@ public class MoneyTest {
      ***********************************************************************
      * Add all your JUnit tests for the Money class below.
      */
-
+    
+    @Test
+    public void testSubtract() {
+    	Money val1 = new Money("30.64");
+    	Money val2 = new Money("10.01");
+    	Money val3 = new Money("-10.01");
+    	Money result1 = val1.subtract(val2);
+    	Money result2 = val1.subtract(val3);
+    	assertEquals("20.63", result1.toString());
+    	assertEquals("40.65", result2.toString());
+    }
+    
+    @Test
+    public void testAddPercent() {
+    	Money val1 = new Money("10.00");
+    	Money val2 = new Money("-10.00");
+    	double percent = 20;
+    	Money result1 = val1.addPercent(percent);
+    	Money result2 = val2.addPercent(percent);
+    	assertEquals("12.00", result1.toString());
+    	assertEquals("-12.00", result2.toString());
+    }
+    
+    @Test
+    public void testCompareTo() {
+    	Money val1 = new Money("33.66");
+    	Money val2 = new Money("33.66");
+    	Money val3 = new Money("10.00");
+    	Money val4 = new Money("-20.00");
+    	int result1 = val1.compareTo(val2);
+    	int result2 = val1.compareTo(val3);
+    	int result3 = val3.compareTo(val1);
+    	int result4 = val4.compareTo(val1);
+    	assertEquals(0, result1);
+    	assertEquals(1, result2);
+    	assertEquals(-1 ,result3);
+    	assertEquals(-1 ,result4);
+    }
+    
+    @Test
+    public void testLessEqual() {
+    	Money val1 = new Money("33.66");
+    	Money val2 = new Money("33.66");
+    	Money val3 = new Money("10.00");
+    	Money val4 = new Money("-10.00");
+    	boolean result1 = val1.lessEqual(val2);
+    	boolean result2 = val1.lessEqual(val3);
+    	boolean result3 = val3.lessEqual(val1);
+    	boolean result4 = val4.lessEqual(val1);
+    	assertEquals(true, result1);
+    	assertEquals(false, result2);
+    	assertEquals(true,result3);
+    	assertEquals(true,result4);
+    }
+    
+    @Test
+    public void testEquals() {
+    	Money val1 = new Money("30.64");
+    	Money val2 = new Money("10.01");
+    	Money val3 = new Money("10.01");
+    	Money val4 = new Money("-10.00");
+    	Money val5 = new Money("-10.00");
+    	boolean result1 = val1.equals(val2);
+    	boolean result2 = val2.equals(val3);
+    	boolean result3 = val4.equals(val5);
+    	assertEquals(false, result1);
+    	assertEquals(true, result2);
+    	assertEquals(true, result3);
+    }
 
     /*
      * Put all class modifications above.
